d620a6f75dd0d4257435117b5e795dd375ad3d4b
bic4907 <bic4907@gmail.com>
2021-03-24 07:03:23 +0900

    Position defined with loop

--- components/view.py
+++ components/view.py
@@ -8,33 +8,28 @@ class View(object):
     def get_visible_positions(direction: DirectionType) -> [Position]:
         direction = direction.get_type()
 
+        positions = []
         if direction == DirectionType.Up:
-            positions = [
-                Position(x=-1, y=3), Position(x=0, y=3), Position(x=1, y=3),
-                Position(x=-1, y=2), Position(x=0, y=2), Position(x=1, y=2),
-                Position(x=-1, y=1), Position(x=0, y=1), Position(x=1, y=1),
-                Position(x=-1, y=0), Position(x=0, y=0), Position(x=1, y=0),
-                Position(x=-1, y=-1), Position(x=0, y=-1), Position(x=1, y=-1),
-            ]
+
+            for x in range(-5, 6):
+                for y in range(9, -2, -1):
+                    positions.append(Position(x=x, y=y))
+
         elif direction == DirectionType.Down:
-            positions = [
-                Position(x=-1, y=1), Position(x=0, y=1), Position(x=1, y=1),
-                Position(x=-1, y=0), Position(x=0, y=0), Position(x=1, y=0),
-                Position(x=-1, y=-1), Position(x=0, y=-1), Position(x=1, y=-1),
-                Position(x=-1, y=-2), Position(x=0, y=-2), Position(x=1, y=-2),
-                Position(x=-1, y=-3), Position(x=0, y=-3), Position(x=1, y=-3),
-            ]
+
+            for x in range(-5, 6):
+                for y in range(1, -10, -1):
+                    positions.append(Position(x=x, y=y))
+
         elif direction == DirectionType.Left:
-            positions = [
-                Position(x=-3, y=1), Position(x=-2, y=1), Position(x=-1, y=1), Position(x=0, y=1), Position(x=1, y=1),
-                Position(x=-3, y=0), Position(x=-2, y=0), Position(x=-1, y=0), Position(x=0, y=0), Position(x=1, y=0),
-                Position(x=-3, y=-1), Position(x=-2, y=-1), Position(x=-1, y=-1), Position(x=0, y=-1), Position(x=1, y=-1),
-            ]
+
+            for x in range(-9, 2):
+                for y in range(-5, 6, 1):
+                    positions.append(Position(x=x, y=y))
         elif direction == DirectionType.Right:
-            positions = [
-                Position(x=-1, y=1), Position(x=0, y=1), Position(x=1, y=1), Position(x=2, y=1), Position(x=3, y=1),
-                Position(x=-1, y=0), Position(x=0, y=0), Position(x=1, y=0), Position(x=2, y=0), Position(x=3, y=0),
-                Position(x=-1, y=-1), Position(x=0, y=-1), Position(x=1, y=-1), Position(x=2, y=-1), Position(x=3, y=-1),
-            ]
+
+            for x in range(-1, 10):
+                for y in range(-5, 6, 1):
+                    positions.append(Position(x=x, y=y))
 
         return positions
