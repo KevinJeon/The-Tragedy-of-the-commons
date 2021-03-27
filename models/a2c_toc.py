64a21926c3b7a000d60cfb2c066d61b582165705
kevinjeon <kevinjeon119@gmail.com>
2021-03-27 11:56:44 +0900

    Add Storage

--- models/a2c_toc.py
+++ models/a2c_toc.py
@@ -19,42 +19,10 @@ class CPC(nn.Module):
         self.linear = nn.Modulelist([nn.Linear(128, 512) for i in range(timestep)])
         self.policy = nn.Linear(128, num_action)
         self.value = nn.Linear(128, 1)
+
     def init_hidden(self, batch_size):
         return tr.zeros(1, batch_size, 128)
 
-    def forward(self, obss, h, acts):
-        '''
-        x : (seq, batch, c, h, w)
-        h : (1, batch, hidden)
-        act : (seq, batch, num_action)
-        '''
-        seq, batch, c, h, w = obss.size()
-        # random t
-        obs = obss.view(seq * batch, c, h, w)
-        t = tr.randint(self.seq_len - self.timestep, size=(1,)).long() # range for 0 ~ seq - timestep
-        z = self.encoder(obss)
-        z_ts = tr.empty((self.timestep, batch, 512))
-        for i in range(self.timestep):
-            z_ts[i] = z[: , t+i, :].view(batch, 512)
-        z_lstm = []
-        for i in range(self.seq_len + 1):
-            out, h = self.lstm(z[i], h)
-            z_lstm.append(out)
-        c_t = z_lstm[t].squeeze(1)
-        c = tr.stack(tensors=z_lstm, dim=0)
-        pi = self.policy(c)
-        v = self.value(c)
-        pred = tr.empty((self.timestep, batch, 512)).float()
-        for i in range(self.timestep):
-            linear = self.linear[i]
-            pred[i] = linear(c_t)
-        nce = 0
-        for i in range(self.timestep):
-            loss = tr.mm(z_ts[i], pred[i].transpose(1,0))
-            nce += tr.sum(tr.diag(F.log_softmax(loss)))
-        nce /= -1 * batch * self.timestep
-        return nce, hidden, pi, v
-    
     def pi(self, obs, h):
         '''
         x : (seq, c, h, w)
