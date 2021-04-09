01002806f7d039f3cff71120354abd7852ae1ef3
kevinjeon <kevinjeon119@gmail.com>
2021-04-09 12:08:09 +0900

    Debug Rulebased Agent

--- models/rulebased.py
+++ models/rulebased.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-class RuleBasedAgent(object):
+class RuleBasedAgent:
 
     def __init__(self, red_apple, blue_apple, justice, obs_size):
         # agent characteristics
@@ -8,10 +8,12 @@ class RuleBasedAgent(object):
         self.justice = justice # bool type to test inequity aversion
         
         # constant
-        self.a = ((obs_size[0] + 1)/2 , 1)
+        self.a = (int((obs_size[0] + 1)/2) , 1)
         self.obs_size = obs_size
-        self.movable = [(self.a[0] + 1, self.a[1]), (self.a[0] - 1, self.a[1]), (self.a[0], self.a[1] + 1), self.a[0], self.a[1] -1)] 
+        self.movable = [(self.a[0] + 1, self.a[1]), (self.a[0] - 1, self.a[1]), (self.a[0], self.a[1] + 1), (self.a[0], self.a[1] -1)] 
         self.rule_order = [self._eat_apple, self._closest_apple, self._explore] 
+        self.color = dict(red=3, blue=2)
+        self.is_train = False
     def act(self, obs):
         # Each step, agent find the most closest apple. 
         
@@ -21,26 +23,29 @@ class RuleBasedAgent(object):
         while act == None:
             act = self.rule_order[o](obs)
             o += 1
-        return act
+        return act, None
 
-    def _move_most_apple(self, obs):
+    def _move_most_apple(self, obs, color='red'):
         # get the index of apples 
         ## to develop to output action
-        apples = np.where(obs == 2)
+        apples = np.where(obs == self.color[color])
         return apples
 
-    def _closest_apple(self, obs):
+    def _closest_apple(self, obs, color='red'):
         # get the index of closest apple
-        apples = np.where(obs == 2)
+        print(obs)
+        apples = np.where(obs == self.color[color])
+        xs, ys = apples[0], apples[1]
         closest = None
         lowest = 1e5
-        for apple in apples:
-            dist = np.linalg.norm(apple - self.a)
+        if len(xs) == 0:
+            return None
+        for x, y in zip(xs, ys):
+            arr = np.array([x,y])
+            dist = np.linalg.norm(arr - self.a)
             if dist < lowest:
                 lowest = dist
                 closest = apple
-        if len(apples) == 0:
-            return None
         else:
             lr, ud = self.a - closest
             candidates = []
@@ -52,32 +57,32 @@ class RuleBasedAgent(object):
                 candidates.append(3)
             if ud < 0:
                 candidates.append(1)
-            elif ud = 0:
+            elif ud == 0:
                 pass
             else:
                 candidates.append(0)
             return np.random.choice(candidates, 1)[0]
 
-    def _explore(self, direction):
+    def _explore(self, obs, color=None):
         # if there is no apple in sight, explore
         horizontal, vertical = [0, self.obs_size[0] - 1], [self.obs_size[1] - 1, 0] # Left, Right, Up, Down
         ## check plz obs indices mean x and y each
         # Check whether wall or not
         possible = []
-        for i, (ud, zr) in enumerate(zip(vertical, horizontal)):
-            if not all(obs[ud, :]) == 1:
+        for i, (ud, lr) in enumerate(zip(vertical, horizontal)):
+            if not np.mean(obs[ud, :]) == 1:
                 possible.append(i)
-            if not all(obs[:, lr]) == 1:
+            if not np.mean(obs[:, lr]) == 1:
                 possible.append(i + 2)
         act = np.random.choice(possible, 1)[0]
         return act
         
         
-    def _eat_apple(self, obs):
+    def _eat_apple(self, obs, color='red'):
         candidates = []
         utilities = []
-        for move in self._movable:
-            is_apple = obs[move[0], move[1]] == 2
+        for move in self.movable:
+            is_apple = obs[move[0], move[1]] in [2, 3] 
             if is_apple:
                 # considering red, blue
                 candidates.append(move)
