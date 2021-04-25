4742c9aa330659b1dbcc4c4da12fcf4a48c8abf8
bic4907 <bic4907@gmail.com>
2021-04-25 23:42:35 +0900

    Rule-based Agent completed

--- test/rule_based_agent.py
+++ test/rule_based_agent.py
@@ -11,11 +11,12 @@ from models.RuleBasedAgent import RuleBasedAgent
 
 def main():
 
-    agents_types = ['red']
+    agents_types = ['red', 'blue']
 
     env = TOCEnv(agents=agents_types,
                  map_size=(16, 16),
                  obs_type='numeric',
+                 patch_count=8,
                  apple_color_ratio=0.5,
                  apple_spawn_ratio=0.1,
                  )
@@ -34,10 +35,10 @@ def main():
             actions = [agent.act(obs) for agent, obs in zip(agents, state)]
 
             next_state, reward, done, info = env.step(actions=actions)
-
+            if True in done: exit(1)
+            print(info)
             image = env.render(coordination=True)
-            cv.imshow('Env', image)
-            cv.waitKey(1)
+
             state = next_state
 
 if __name__ == '__main__':
