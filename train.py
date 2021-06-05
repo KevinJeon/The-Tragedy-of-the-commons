942dd37874c2962005f1ac45c76403c850af6f35
Baek In-Chang <inchang.baek@gm.gist.ac.kr>
2021-06-05 15:34:20 +0900

    Add SVO and make_pool

--- train.py
+++ train.py
@@ -12,20 +12,9 @@ from models.utils.RolloutStorage import RolloutStorage
 from recorder import VideoRecorder
 from tocenv.env import *
 from utils.logging import *
+from utils.svo import svo
 import numpy as np
 
-def svo(rews, aind):
-    exc_rews = (np.sum(rews) - rews[aind]) / (len(rews) - 1)
-    my_rew = rews[aind]
-    if my_rew == 0:
-        rew_angle = np.pi / 2
-    else:
-        rew_angle = np.arctan(exc_rews / my_rew)
-
-    target_angle = np.pi / 2
-    w = 0.2
-    U = my_rew - w * abs(target_angle - rew_angle)
-    return U
 
 class Workspace(object):
     def __init__(self, cfg):
@@ -71,7 +60,8 @@ class Workspace(object):
             self.replay_buffer = RolloutStorage(agent_type='ac',
                                                 num_agent=cfg.env.blue_agent_count + cfg.env.red_agent_count,
                                                 num_step=cfg.env.episode_length,
-                                                batch_size=cfg.agent.batch_size, num_obs=(8 * self.obs_dim, 8 * self.obs_dim, 3), num_action=8,
+                                                batch_size=cfg.agent.batch_size,
+                                                num_obs=(8 * self.obs_dim, 8 * self.obs_dim, 3), num_action=8,
                                                 num_rec=128)
 
         # self.writer = SummaryWriter(log_dir="tb")
@@ -192,7 +182,6 @@ class Workspace(object):
 
             done = True in dones
             if episode_step >= self.env.episode_length:
-                print(episode_step, self.env.episode_length, 'oot')
                 done = True
 
             episode_reward += sum(rewards)
